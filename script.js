bfe0132d8bce7553b67d1b952c71a5e79a24dca7
freire <luancfreire@gmail.com>
2022-06-04 12:27:17 -0300

    incrementando o btn wpp e adicionado o arquivo .js

---
+++ script.js
@@ -0,0 +1,14 @@
+class Programa{
+
+    constructor(){}
+    
+    pegarDadosForms(){
+        let nome = document.getElementById('nome').value;
+        let numeroTel = document.getElementById('numeroTel').value;
+        let email = document.getElementById('email').value;
+
+        console.log(nome, numeroTel, email);
+    }
+    
+
+}
